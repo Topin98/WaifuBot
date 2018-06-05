43f4639905d9c5f47959db757c1200edbb071d30
Daniel Garcia <marielina98@hotmail.com>
2018-06-05 14:43:52 +0200

    1.0

--- commands/invitar.js
+++ commands/invitar.js
@@ -1,5 +1,7 @@
+//invita a un usuario al guild (el bot manda por md la invitacion)
 exports.run = (client, message, args, sql) => {
 
+    //el unico argumento va a ser el ID del usuario al que se va a invitar
     if (args.length == 1){
 
         message.guild.channels.get(message.channel.id).createInvite().then(invite =>
